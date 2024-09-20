09e335f5aa8467c1bfaede1cc2f4504aa13c5041
Abdelilah Ouechmi <abdofatal25@gmail.com>
2024-09-20 11:03:43 +0100

    commit-6

---
+++ github-user-search/tailwind.config.js
@@ -0,0 +1,10 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Adjust as necessary for your project structure
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
